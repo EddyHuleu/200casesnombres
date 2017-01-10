07c5119252f1a87db3eb3871dae1972b61cafd16
Eddy Huleu <eddy.huleu@live.fr>
2017-01-10 15:41:45 +0100

    conditions ajoutés

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-const seuil=200;
-
-for (var i = 1; i <= seuil; i++) {
+var nbCase = prompt("Combien de case souhaitez-vous?");	
+if(nbCase < 1000){
+for (var i = 1; i <= nbCase; i++) {
 	document.getElementById('liste').innerHTML += "<li>" +i+ "</li>";
-}	
+}}else{
+	alert("nombre trop élevé")
+}
+     
