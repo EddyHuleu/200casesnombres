8c0e2113d88c4aa6e4231e3b34553a7b665620d6
Eddy Huleu <eddy.huleu@live.fr>
2017-01-10 15:01:35 +0100

    modif all

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-for (var i = 1;  i  < 200; i++){
-	document.write("<li>", i, "</li>")
-}
+const seuil=200;
+
+for (var i = 1; i <= seuil; i++) {
+	document.getElementById('liste').innerHTML += "<li>" +i+ "</li>";
+}	
